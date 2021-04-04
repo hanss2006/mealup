ce86119ace3eca71345e20647f18694730a7c47c
hanss2006 <hanss@live.ru>
2021-04-04 21:07:52 +0300

    Meal Component

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8080/meal_up_backend_war'
-export const JPA_API_URL = 'http://localhost:8080/meal_up_backend_war/jpa'
+export const API_URL = 'http://192.168.5.45:9080/meal_up_backend_war'
+export const JPA_API_URL = 'http://192.168.5.45:9080/meal_up_backend_war/jpa'
